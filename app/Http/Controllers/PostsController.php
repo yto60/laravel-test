a3b7d30a757bce721488955bdde2af6158d77fe0
60° <hatsumi.yamane.4609@gmail.com>
2020-02-23 07:24:26 +0900

    Hello Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         // dd($post); // debug
 
-        // if (!array_key_exists($slug, $posts)) {
-        //     abort(404, 'Sorry, that page was not found.');
+        // when using first()
+        // if (!$post) {
+        //     abort(404);
         // }
 
         return view('post', [
